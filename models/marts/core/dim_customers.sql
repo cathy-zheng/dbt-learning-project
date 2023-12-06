896949f4411a40621b73bcec241d79d782bcbb5e
cathy1994zh@gmail.com <cathy1994zh@gmail.com>
2023-12-06 20:55:44 +0000

    adding lifetime value

--- models/marts/core/dim_customers.sql
+++ models/marts/core/dim_customers.sql
@@ -1,41 +1,25 @@
-{{config(
-    materialized='table'
-)}}
-
 with customers as (
 
-    select
-        customer_id,
-        first_name,
-        last_name
-
-    from {{ ref("stg_customers") }}
+    select * from {{ ref("stg_customers") }}
 
 ),
 
 orders as (
 
-    select
-        order_id,
-        customer_id,
-        order_date,
-        status
-
-    from {{ ref("stg_orders") }}
+    select * from {{ ref("fct_orders") }}
 
 ),
 
 customer_orders as (
 
     select
-        customer_id,
-
-        min(order_date) as first_order_date,
-        max(order_date) as most_recent_order_date,
-        count(order_id) as number_of_orders
-
-    from orders
-
+        orders.customer_id,
+        min(orders.order_date) as first_order_date,
+        max(orders.order_date) as most_recent_order_date,
+        count(orders.order_id) as number_of_orders,
+        sum(orders.amount) as lifetime_value
+    from customers
+    left join orders using (customer_id)
     group by 1
 
 ),
@@ -49,12 +33,10 @@ final as (
         customers.last_name,
         customer_orders.first_order_date,
         customer_orders.most_recent_order_date,
-        coalesce(customer_orders.number_of_orders, 0) as number_of_orders
-
+        coalesce(customer_orders.number_of_orders, 0) as number_of_orders,
+        coalesce(customer_orders.lifetime_value, 0) as lifetime_value
     from customers
-
     left join customer_orders using (customer_id)
-
 )
 
 select * from final
